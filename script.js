648a551215094cda76da5e7557214d9c0098dee2
ekane1 <ekane@eagles.ewu.edu>
2016-06-09 11:58:53 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+var $G = $('.G');
+TweenMax.from($G, 1.5, {
+    
+    opacity: 0,
+    x: -600   
+});
+
+var $o01 = $('.o01');
+TweenMax.from($o01, 1.5, {
+    
+    opacity: 0,
+    y: -300   
+});
+
+var $o02 = $('.o02');
+TweenMax.from($o02, 1.5, {
+    
+    opacity: 0,
+    y: 500,
+    x: 300
+});
+
+var $gg = $('.gg');
+TweenMax.from($gg, 1.5, {
+    
+    opacity: 0,
+    x: -200,
+    y: 600
+});
+
+var $l = $('.l');
+TweenMax.from($l, 1.5, {
+    
+    opacity: 0,
+    x: 400,
+    y: -300
+});
+
+var $e = $('.e');
+TweenMax.from($e, 1.5, {
+    
+    opacity: 0,
+    x: 400,
+});
